16dd014678c5b53e40aa221f0a15c90d5ef32f75
Juliur9 <url.julian@gmail.com>
2025-12-05 15:38:54 +0100

    Routen hinzugefügt

--- frontend/TeamPanel-Frontend/src/App.jsx
+++ frontend/TeamPanel-Frontend/src/App.jsx
@@ -1,37 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Components/Home";
+import Error from "./Components/Error";
+
 function App() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center w-full h-screen">
-        <div>
-          <h1 className="m-5 text-xl">TeamPanel</h1>
-        </div>
-        <div>
-          <p>Welcome to TeamPanel Frontend!</p>
-        </div>
-        <div className="grid grid-cols-4 gap-2 mt-5 ">
-          <SectionPanel title={"Test"} description={"description test"} />
-          <SectionPanel />
-          <SectionPanel />
-          <SectionPanel />
-          <SectionPanel />
-          <SectionPanel />
-          <SectionPanel />
-          <SectionPanel />
-        </div>
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Error />} /> //error
+        </Routes>
+      </Router>
     </>
   );
 }
 
 export default App;
-
-function SectionPanel({ title, description }) {
-  return (
-    <section className="w-32 h-64 m-1 mt-5 transition-transform bg-slate-100 hover:scale-105">
-      <div className="flex flex-col items-center justify-center h-full">
-        <h2 className="mb-2 text-lg">{title}</h2>
-        <p className="text-sm text-center">{description}</p>
-      </div>
-    </section>
-  );
-}
---
+++ frontend/TeamPanel-Frontend/src/Components/Home.jsx
@@ -0,0 +1,40 @@
+export default function Home() {
+  return (
+    <>
+      <div>
+        <nav className="flex w-full p-8 bg-slate-400">
+          <a href="/login">Login</a>
+        </nav>
+        <div className="flex flex-col items-center justify-center w-full h-screen">
+          <div>
+            <h1 className="m-5 text-xl">TeamPanel</h1>
+          </div>
+          <div>
+            <p>Welcome to TeamPanel Frontend!</p>
+          </div>
+          <div className="grid grid-cols-4 gap-2 mt-5 ">
+            <SectionPanel title={"Test"} description={"description test"} />
+            <SectionPanel />
+            <SectionPanel />
+            <SectionPanel />
+            <SectionPanel />
+            <SectionPanel />
+            <SectionPanel />
+            <SectionPanel />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function SectionPanel({ title, description }) {
+  return (
+    <section className="w-32 h-64 m-1 mt-5 transition-transform bg-slate-100 hover:scale-105">
+      <div className="flex flex-col items-center justify-center h-full">
+        <h2 className="mb-2 text-lg">{title}</h2>
+        <p className="text-sm text-center">{description}</p>
+      </div>
+    </section>
+  );
+}
